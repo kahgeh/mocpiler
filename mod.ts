904fc1bdb1da61221d5b67da6260b3e3f66b8594
kahgeh <kahgeh@hotmail.com>
2022-03-08 21:56:12 +1100

    pick exports

---
+++ mod.ts
@@ -0,0 +1,13 @@
+export { Method } from './types.ts';
+
+export type {
+  Request,
+  Service,
+  IBuilder,
+  RequestReponse,
+  DatasetBuildInfo,
+  ServicesRequestResponses,
+} from './types.ts';
+
+export { DatasetBuilder } from './datasetBuilder.ts';
+export { compile } from './compile.ts';
